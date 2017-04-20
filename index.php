4bb3ac38ba11d649720af36d9dca166c229de0d4
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-04-20 17:58:13 -0300

    index

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php require_once 'config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assinar newsletter</title>
+	<style type="text/css">
+		body{
+			font-family: 'Trebuchet MS';
+			background-color: #50587f;
+		}
+		.form{
+			width: 400px;
+			height: auto;
+			margin: auto;
+			margin-top: 10%;
+			border: 5px dotted #dedfe5;
+			border-radius: 5px;
+			text-align: center;
+			padding: 10px;
+			color: #dedfe5;
+		}
+		input{
+			width: 80%;
+			height: 20px;
+			font-weight: bold;
+			color: #384072;
+			font-family: 'Trebuchet MS';
+			border-radius: 5px;
+			padding: 2px;
+		}
+		.btn{
+			height: 40px;
+			font-size: 14pt;
+		}
+	</style>
+</head>
+<body>
+<?php 
+	require_once 'assinar.php';
+
+
+//	$newsletter = new assinardao();
+//	$newsletter->assinar("salumao","salumao@gmail.com");
+ ?>
+</body>
+</html>
